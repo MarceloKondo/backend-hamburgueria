9f68663b8d7bbf979c28546f3518edcb05f9faf2
MarceloKondo <marcelo.skondo@gmail.com>
2026-04-24 14:15:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ startServer();
 app.get("/api/v1/app/versao", async (req, res) => {
     try {
         res.json({
-            versionCode: 2,
+            versionCode: 3,
             versionName: "1.0.0",
             forceUpdate: true,
             apkUrl: "https://hamburgueria-api-74br.onrender.com/Dominus.apk",
